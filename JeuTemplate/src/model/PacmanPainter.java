bde8f2a341b23f99ff6c0535d2e925945e4cf939
esteban <esteban.mrt15@gmail.com>
2021-11-04 10:18:42 +0100

    personnage1

--- JeuTemplate/src/model/PacmanPainter.java
+++ JeuTemplate/src/model/PacmanPainter.java
@@ -2,9 +2,17 @@ package model;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
+
 import engine.GamePainter;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 /**
  * @author Horatiu Cirstea, Vincent Thomas
@@ -17,8 +25,8 @@ public class PacmanPainter implements GamePainter {
 	/**
 	 * la taille des cases
 	 */
-	protected static final int WIDTH = 100;
-	protected static final int HEIGHT = 100;
+	protected static final int WIDTH = 1000;
+	protected static final int HEIGHT = 1000;
 
 	/**
 	 * appelle constructeur parent
@@ -34,9 +42,22 @@ public class PacmanPainter implements GamePainter {
 	 */
 	@Override
 	public void draw(BufferedImage im) {
-		Graphics2D crayon = (Graphics2D) im.getGraphics();
-		crayon.setColor(Color.blue);
-		crayon.fillOval(0,0,10,10);
+		Graphics2D fond = (Graphics2D) im.getGraphics();
+		Graphics2D player = (Graphics2D) im.getGraphics();
+		
+		Image image = null;
+		try {
+			image = ImageIO.read(new File("C:\\Users\\esteb\\Documents\\ensem\\2A\\analyse et conception de logiciels\\projet\\ACL2021-Les_codeurs_du_dimanche\\JeuTemplate\\map.png"));
+			fond.drawImage(image, 0, 0,1000,1000, null);
+			image = ImageIO.read(new File("C:\\Users\\esteb\\Documents\\ensem\\2A\\analyse et conception de logiciels\\projet\\ACL2021-Les_codeurs_du_dimanche\\JeuTemplate\\pacman.png"));
+			player.drawImage(image, 0, 0, 50, 50, null);
+			
+		} 
+		catch (IOException e) {
+
+			player.drawString("Image inexistante", 0, 0);  }
+		
+		
 	}
 
 	@Override
